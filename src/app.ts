4ca49cee31b702092af9adae611f754899777228
wanderer06 <georgica_fara_de_frica@yahoo.com>
2017-01-05 17:22:44 +0000

    Add basic ability to configure initial set

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,12 @@
 import Fractal from './fractal';
 
-const fractal = new Fractal(100, 6);
-fractal.match('reference/Mona-Lisa-200x200.jpg');
+let fractal: Fractal;
+// hacky logic for now
+
+document.getElementById('start_match').addEventListener('click', () => {
+  const polygons = (<HTMLInputElement>document.getElementById('polygons')).value;
+  const vertices = (<HTMLInputElement>document.getElementById('vertices')).value;
+
+  fractal = new Fractal(parseInt(polygons), parseInt(vertices));
+  fractal.match('reference/Mona-Lisa-200x200.jpg');
+});
